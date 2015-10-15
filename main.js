8ea3bb90f6854e57ffe71dbd909614f1192a84cd
Rhys Evans <rhys.evans@ft.com>
2015-10-15 14:31:00 +0100

    cleverer routing of upstream errors to splunk/sentry

--- main.js
+++ main.js
@@ -1,12 +1,49 @@
 'use strict';
 
 const logger = require('ft-next-logger');
+const fetchres = require('fetchres');
+const raven = require('raven');
+let ravenMiddleware;
+
+function sendErrorDev (err, req, res) {
+	if (err.name === fetchres.ReadTimeoutError.name) {
+		logger.error('Dependency timeout -', err);
+		res && res.status(504).send({ type: 'Bad Gateway', error: err });
+	} else {
+		logger.error('Uncaught Error -', err);
+		res && res.status(500).send({ type: 'Uncaught Error', error: err });
+		process.exit(1);
+	}
+}
+
+function sendErrorProd (err, req, res) {
+	if (err.name === fetchres.ReadTimeoutError.name) {
+		logger.error('Dependency timeout -', err);
+		res && res.status(504).send({ type: 'Bad Gateway', error: err });
+	} else {
+		return ravenMiddleware(err, req, res);
+	}
+}
+
+function getUpstreamErrorHandler (errorReporter) {
+	return function(res, statusCode) {
+		return function(err) {
+			if (err.name === fetchres.BadServerResponseError.name) {
+				errorReporter(err);
+				res.status(statusCode).end();
+			} else {
+				errorReporter(err, null, res);
+			}
+		};
+	}
+}
 
 if (process.env.NODE_ENV === 'production') {
-	const raven = require('raven');
 	const client = new raven.Client(process.env.RAVEN_URL);
 	module.exports = client;
-	module.exports.middleware = raven.middleware.express(client);
+	module.exports.middleware = sendErrorProd;
+	module.exports.upstreamErrorHandler = getUpstreamErrorHandler(sendErrorProd);
+	ravenMiddleware = raven.middleware.express(client);
 
 	// Die on uncaughtException
 	// https://github.com/getsentry/raven-node#catching-global-errors
@@ -14,6 +51,7 @@ if (process.env.NODE_ENV === 'production') {
 		process.exit(1);
 	});
 } else {
+
 	module.exports = {
 		captureMessage: function () {
 			logger.warn.apply(logger, arguments);
@@ -21,10 +59,7 @@ if (process.env.NODE_ENV === 'production') {
 		captureError: function () {
 			logger.warn.apply(logger, arguments);
 		},
-		middleware: function(err, req, res) {
-			logger.error('Uncaught Error -', err);
-			res.status(500).send({ type: 'Uncaught Error', error: err });
-			process.exit(1);
-		}
+		middleware: sendErrorDev,
+		upstreamErrorHandler: getUpstreamErrorHandler(sendErrorDev)
 	};
 }
