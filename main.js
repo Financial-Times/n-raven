049b2b970d0f6f8ca3c8bd74bbef1e0d94899c3a
Rhys Evans <rhys.evans@ft.com>
2017-10-05 11:52:30 +0100

    update raven client and remove underused methods  🐿 v2.5.16

--- main.js
+++ main.js
@@ -2,69 +2,8 @@
 'use strict';
 
 const logger = require('@financial-times/n-logger').default;
-const fetchres = require('fetchres');
 const raven = require('raven');
 const path = require('path');
-let ravenMiddleware;
-
-function sendErrorDev (err, req, res, next) {
-	let error;
-	if (err instanceof Error) {
-		error = {
-			name: err.name,
-			message: err.message
-		};
-		if (err.stack) {
-			error.stack = err.stack.split('\n');
-		}
-	} else {
-		error = err;
-	}
-	if (err.name === fetchres.ReadTimeoutError.name) {
-		logger.error(err, { event: 'dependencytimeout' });
-		res && res.status(504).send({ type: 'Bad Gateway', error: error });
-	} else {
-		logger.error(err, { event: 'uncaughterror' });
-		res && res.status(500).send({ type: 'Uncaught Error', error: error });
-	}
-}
-
-function sendErrorProd (err, req, res, next) {
-	if (err.name === fetchres.ReadTimeoutError.name) {
-		logger.error(err, { event: 'dependencytimeout' });
-		res && res.status(504).send({ type: 'Bad Gateway', error: err });
-	} else {
-		logger.error(err, { event: 'uncaughterror' });
-		if (req && res && next) {
-			err = sanitiseError(err);
-			return ravenMiddleware(err, req, res, next);
-		}
-	}
-}
-
-function getUpstreamErrorHandler (errorReporter) {
-	return function (res, next, statusCode) {
-		return function (err) {
-
-			if (err.name === fetchres.BadServerResponseError.name) {
-				errorReporter(err);
-				res.status(statusCode).end();
-			} else {
-				next(err);
-			}
-		};
-	};
-}
-
-function getCaptureError (client, _captureError) {
-	return function (err) {
-		if (err.name === fetchres.ReadTimeoutError.name) {
-			logger.error(err, { event: 'dependencytimeout' });
-		} else {
-			_captureError.apply(client, arguments);
-		}
-	};
-}
 
 function sanitiseError (err) {
 	//Initially check for a specific email field e.g. in a url in an attempt to not blitz helpful error information if possible
@@ -114,35 +53,44 @@ if (process.env.NODE_ENV === 'production') {
 		about = {};
 	}
 
-	const client = new raven.Client(process.env.RAVEN_URL, {
-		release: about.appVersion || 'unknown',
-		name: about.description || 'unknown',
-		tags: {
-			buildTime: about.buildCompletionTime || 'unknown'
-		}
-	});
+	raven
+		.config(process.env.RAVEN_URL, {
+			release: about.appVersion || 'unknown',
+			name: about.description || 'unknown',
+			tags: {
+				buildTime: about.buildCompletionTime || 'unknown'
+			}
+		})
+		// Die on uncaughtException
+		// https://docs.sentry.io/clients/node/usage/#global-fatal-error-handler
+		.install(() => process.exit(1));
 
-	const _captureError = client.captureError;
-	module.exports = client;
-	module.exports.captureError = getCaptureError(client, _captureError);
+	module.exports = raven;
 	module.exports.middleware = sendErrorProd;
-	module.exports.upstreamErrorHandler = getUpstreamErrorHandler(sendErrorProd);
-
-	ravenMiddleware = raven.middleware.express(client);
-
-	// Die on uncaughtException
-	// https://github.com/getsentry/raven-node#catching-global-errors
-	client.patchGlobal(function () {
-		process.exit(1);
-	});
 
 } else {
 
 	module.exports = {
+		context: func => func(),
 		captureMessage: logger.warn.bind(logger),
 		captureError: logger.error.bind(logger),
-		middleware: sendErrorDev,
-		upstreamErrorHandler: getUpstreamErrorHandler(sendErrorDev)
+		requestHandler: () => (req, res, next) => next(),
+		errorHandler: () => (err, req, res, next) => {
+			let error;
+			if (err instanceof Error) {
+				error = {
+					name: err.name,
+					message: err.message
+				};
+				if (err.stack) {
+					error.stack = err.stack.split('\n');
+				}
+			} else {
+				error = err;
+			}
+			logger.error(err, { event: 'uncaughterror' });
+			res && res.status(500).send({ type: 'Uncaught Error', error: error });
+		}
 	};
 }
 
